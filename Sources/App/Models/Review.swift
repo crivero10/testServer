c11c2123791b094911987c582f71b7dd7bb037e4
crivero10 <2700c.ri@gmail.com>
2019-09-19 03:06:48 -0500

    first commit

---
+++ Sources/App/Models/Review.swift
@@ -0,0 +1,35 @@
+//
+//  Review.swift
+//  App
+//
+//  Created by master on 19/09/19.
+//
+
+import Foundation
+import Vapor
+import FluentMySQL
+
+final class Review: MySQLModel{
+    var id: Int?
+    var username: String
+    var text: String
+    var movie_id: String
+
+    /// Creates a new `Todo`.
+    init(id: Int? = nil, username: String, text: String, movie_id: String) {
+        self.id = id
+        self.username = username
+        self.text = text
+        self.movie_id = movie_id
+    }
+}
+
+
+extension Review: Migration { }
+
+/// Allows `Todo` to be encoded to and decoded from HTTP messages.
+extension Review: Content { }
+
+/// Allows `Todo` to be used as a dynamic parameter in route definitions.
+extension Review: Parameter { }
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,13 +8,20 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello", "second", "third") { req in
+        return "Hello, Joe!"
     }
 
     // Example of configuring a controller
     let todoController = TodoController()
+    //when route=/todos, use function 'todoController.index'
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    let reviewController = ReviewController()
+    router.get("reviews", "popular", use: reviewController.indexPopular)
+    router.get("reviews", use: reviewController.index)
+    router.post("reviews", use: reviewController.create)
+    router.delete("reviews", use: reviewController.delete)
 }
